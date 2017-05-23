7b218bfbd416fe67c308a8d8f8549abc8a3b229e
Rodrigo Wirth <rodrigo.wirth@bravi.com.br>
2017-05-23 18:38:27 -0300

    Create game logic

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -13,7 +13,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var rightImageView: UIImageView!
     @IBOutlet weak var leftImageView: UIImageView!
     @IBOutlet weak var leftScoreLabel: UILabel!
-    @IBOutlet weak var rigthScoreLabel: UILabel!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    let cardNames = [
+        "card2",
+        "card3",
+        "card4",
+        "card5",
+        "card6",
+        "card7",
+        "card8",
+        "card9",
+        "card10",
+        "jack",
+        "queen",
+        "king",
+        "ace"
+    ]
+    
+    var leftScore = 0;
+    var rightScore = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +46,19 @@ class ViewController: UIViewController {
 
 
     @IBAction func dealTapped(_ sender: Any) {
-        print("deal tapper")
+        let leftNumber:Int = Int(arc4random_uniform(13))
+        let rightNumber:Int = Int(arc4random_uniform(13))
+        
+        leftImageView.image = UIImage(named: cardNames[leftNumber])
+        rightImageView.image = UIImage(named: cardNames[rightNumber])
+        
+        if leftNumber > rightNumber {
+            leftScore += 1;
+            leftScoreLabel.text = String(leftScore)
+        } else if leftNumber < rightNumber {
+            rightScore += 1;
+            rightScoreLabel.text = String(rightScore)
+        }
     }
 }
 
