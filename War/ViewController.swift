049fe11e88c8569a5bb3fa97479ed5b5214c08f2
Rodrigo Wirth <rodrigo.wirth@bravi.com.br>
2017-05-23 17:57:31 -0300

    Hooking it all up – Swift IBOutlet properties

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    @IBOutlet weak var rigthScoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
